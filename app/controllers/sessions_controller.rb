089666c2de8f39d5d309b58c568c09736e4e0ae8
Alexis <alexis.mariotti@laposte.net>
2025-04-29 22:20:48 +0200

    authetification but missing form error handle

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+  # login form
+  def new
+  end
+
+  # login
+  def create
+    user = User.where(email: params[:email]).first
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id.to_s
+      redirect_to root_path, notice: "Connecté"
+    else
+      flash.now[:alert] = "Email ou mot de passe incorrect"
+      render :new
+    end
+  end
+
+  # logout
+  def destroy
+    session[:user_id] = nil
+    redirect_to login_path, notice: "Déconnecté"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -15,4 +16,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # Routes for user authentication
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
