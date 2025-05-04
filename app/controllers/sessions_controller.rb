6f8758eda5bf47ed1c1eb3fa2b0e588a1c4f1f4f
Alexis <alexis.mariotti@laposte.net>
2025-05-04 20:28:28 +0200

    fixed: authetification errors display

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,23 +2,33 @@ class SessionsController < ApplicationController
 
   # login form
   def new
+    @error_messages = []
   end
 
   # login
   def create
+    @error_messages = []
+
     user = User.where(email: params[:email]).first
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id.to_s
       redirect_to root_path, notice: "Connecté"
     else
-      flash.now[:alert] = "Email ou mot de passe incorrect"
-      render :new
+      puts "YYYYY"
+      # add the error message
+      @error_messages << "Email ou mot de passe incorrect"
+      # and display it
+      respond_to do |format|
+        format.turbo_stream { render "shared/error_messages", locals: { error_messages: ["Email ou mot de passe incorrect"] } }
+        format.html do
+          render :new
+        end
+      end
     end
   end
 
   # logout
   def destroy
-    puts "destroy BBBB"
     session[:user_id] = nil
     redirect_to login_path, notice: "Déconnecté"
   end
